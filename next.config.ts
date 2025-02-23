c1d28c021e2bc625edbc59e007a1776e75ce703e
Jeremy Pinto <jerpint@gmail.com>
2025-02-23 11:01:37 -0600

    add soft redirect

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async redirects() {
-    return [];  // Explicitly return empty redirects
+    return [
+      {
+        source: '/pdf/:id',
+        destination: '/pdf/:id',  // Redirect back to original path
+        permanent: false,         // Use temporary redirect, this is to undo the redirect from the old pdf page
+      },
+    ];
   },
 };
 
