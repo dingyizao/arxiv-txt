f603534357d9df108645998e53cc1f3f5878192c
Jeremy Pinto <jerpint@gmail.com>
2025-02-23 11:34:36 -0600

    fix github link

--- app/pdf/[id]/page.js
+++ app/pdf/[id]/page.js
@@ -20,7 +20,7 @@ export default function HTMLPage({ params }) {
   return (
     <>
       <div className="text-sm border-l-4 border-base-content/20 pl-4 py-1 mb-4 text-base-content/80">
-        This HTML view is experimental. Please report any issues on <a href="https://github.com/arXiv-research/arxiv-research/issues" className="underline">GitHub</a>.
+        The PDF extraction is experimental. Please report any issues on <a href="https://github.com/jerpint/arxiv-txt/issues" className="underline">GitHub</a>.
       </div>
       {paperError || paperContentError ? (
         <div className="card bg-base-100 shadow-xl">
