b8a2f2cf23e0ada39ed36b6f0bdb638ce480069d
Jeremy Pinto <jerpint@gmail.com>
2025-02-25 19:07:30 -0600

    add html endpoint, might as well

---
+++ app/html/[id]/page.js
@@ -0,0 +1,12 @@
+'use client';
+
+import AbstractPage from '@/app/abs/[id]/page';
+
+/**
+ * This page is a duplicate of /abs/[id] endpoint.
+ * The functionality is identical to the /abs/[id] endpoint.
+ */
+
+export default function PDFPage(props) {
+  return <AbstractPage {...props} />;
+}
